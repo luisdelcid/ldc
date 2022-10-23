018740fa4f285bc064316287dc7fc01c777a286d
Vidsoe <labs@vidsoe.com>
2022-10-22 21:34:15 -0600

    LDC

--- readfile.php
+++ readfile.php
@@ -2,26 +2,26 @@
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-function ifwp_404(){
+function ldc_404(){
     status_header(404);
 	die('404 &#8212; File not found.');
 }
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-function ifwp_attachment_file_to_postid($file = ''){
+function ldc_attachment_file_to_postid($file = ''){
 	if(!defined('WP_CONTENT_URL')){
 		define('WP_CONTENT_URL', get_option('siteurl') . '/wp-content');
 	}
     $upload_dir = wp_get_upload_dir();
     $url = str_replace($upload_dir['basedir'], $upload_dir['baseurl'], $file);
-    return ifwp_attachment_url_to_postid($url);
+    return ldc_attachment_url_to_postid($url);
 }
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-function ifwp_attachment_url_to_postid($url = ''){
-    $post_id = ifwp_guid_to_postid($url);
+function ldc_attachment_url_to_postid($url = ''){
+    $post_id = ldc_guid_to_postid($url);
     if($post_id){
         return $post_id;
     }
@@ -31,7 +31,7 @@ function ifwp_attachment_url_to_postid($url = ''){
         if(isset($matches[3])){
             $url .= $matches[3];
         }
-        $post_id = ifwp_guid_to_postid($url);
+        $post_id = ldc_guid_to_postid($url);
         if($post_id){
             return $post_id;
         }
@@ -42,7 +42,7 @@ function ifwp_attachment_url_to_postid($url = ''){
         if(isset($matches[3])){
             $url .= $matches[3];
         }
-        $post_id = ifwp_guid_to_postid($url);
+        $post_id = ldc_guid_to_postid($url);
         if($post_id){
             return $post_id;
         }
@@ -53,7 +53,7 @@ function ifwp_attachment_url_to_postid($url = ''){
         if(isset($matches[3])){
             $url .= $matches[3];
         }
-        $post_id = ifwp_guid_to_postid($url);
+        $post_id = ldc_guid_to_postid($url);
         if($post_id){
             return $post_id;
         }
@@ -63,19 +63,19 @@ function ifwp_attachment_url_to_postid($url = ''){
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-function ifwp_current_user_can($capability = ''){
-    $user_id = ifwp_get_current_user_id();
-    return ifwp_user_can($user_id, $capability);
+function ldc_current_user_can($capability = ''){
+    $user_id = ldc_get_current_user_id();
+    return ldc_user_can($user_id, $capability);
 }
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-function ifwp_dirname($path = '', $levels = 1){
+function ldc_dirname($path = '', $levels = 1){
     $levels = (int) $levels;
     $path = dirname($path);
     if($levels > 1){
         $levels --;
-        return ifwp_dirname($path, $levels);
+        return ldc_dirname($path, $levels);
     } else {
         return $path;
     }
@@ -83,7 +83,7 @@ function ifwp_dirname($path = '', $levels = 1){
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-function ifwp_get_current_user_id(){
+function ldc_get_current_user_id(){
     global $wpdb;
     $siteurl = get_option('siteurl');
     if(!$siteurl){
@@ -107,7 +107,7 @@ function ifwp_get_current_user_id(){
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-function ifwp_get_post($post_id = 0){
+function ldc_get_post($post_id = 0){
     global $wpdb;
 	$query = $wpdb->prepare("SELECT * FROM $wpdb->posts WHERE ID = %d", $post_id);
 	return $wpdb->get_row($query);
@@ -115,7 +115,7 @@ function ifwp_get_post($post_id = 0){
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-function ifwp_get_post_meta($post_id = 0, $key = '', $single = false){
+function ldc_get_post_meta($post_id = 0, $key = '', $single = false){
     global $wpdb;
     $query = $wpdb->prepare("SELECT meta_value FROM $wpdb->postmeta WHERE post_id = %d AND meta_key = %s", $post_id, $key);
     if($single){
@@ -127,22 +127,22 @@ function ifwp_get_post_meta($post_id = 0, $key = '', $single = false){
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-function ifwp_get_post_status($post_id = 0){
-	$post = ifwp_get_post($post_id);
+function ldc_get_post_status($post_id = 0){
+	$post = ldc_get_post($post_id);
 	if(!is_object($post)){
 		return false;
 	}
 	$post_status = $post->post_status;
 	if('attachment' === $post->post_type and 'inherit' === $post_status){
-		if(0 === $post->post_parent or !ifwp_get_post($post->post_parent) or $post->ID === $post->post_parent){
+		if(0 === $post->post_parent or !ldc_get_post($post->post_parent) or $post->ID === $post->post_parent){
 			$post_status = 'publish'; // Unattached attachments with inherit status are assumed to be published.
-		} elseif('trash' === ifwp_get_post_status($post->post_parent)){
-			$post_status = ifwp_get_post_meta($post->post_parent, '_wp_trash_meta_status', true); // Get parent status prior to trashing.
+		} elseif('trash' === ldc_get_post_status($post->post_parent)){
+			$post_status = ldc_get_post_meta($post->post_parent, '_wp_trash_meta_status', true); // Get parent status prior to trashing.
 			if(!$post_status){
 				$post_status = 'publish'; // Assume publish as above.
 			}
 		} else {
-			$post_status = ifwp_get_post_status($post->post_parent);
+			$post_status = ldc_get_post_status($post->post_parent);
 		}
 	} elseif('attachment' === $post->post_type and !in_array($post_status, ['auto-draft', 'private', 'trash'], true)){
 		$post_status = 'publish'; // Ensure uninherited attachments have a permitted status either 'private', 'trash', 'auto-draft'. This is to match the logic in wp_insert_post().
@@ -152,7 +152,7 @@ function ifwp_get_post_status($post_id = 0){
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-function ifwp_guid_to_postid($guid = ''){
+function ldc_guid_to_postid($guid = ''){
     global $wpdb;
     $query = $wpdb->prepare("SELECT ID FROM $wpdb->posts WHERE guid = %s", $guid);
     $post_id = $wpdb->get_var($query);
@@ -164,7 +164,7 @@ function ifwp_guid_to_postid($guid = ''){
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-function ifwp_serve_file($file = ''){
+function ldc_serve_file($file = ''){
     $mime = wp_check_filetype($file);
 	if(false === $mime['type'] and function_exists('mime_content_type')){
 		$mime['type'] = mime_content_type($file);
@@ -185,7 +185,7 @@ function ifwp_serve_file($file = ''){
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-function ifwp_user_can($user_id = 0, $capability = ''){
+function ldc_user_can($user_id = 0, $capability = ''){
     global $wpdb;
     $query = $wpdb->prepare("SELECT meta_value FROM $wpdb->usermeta WHERE user_id = %d AND meta_key = %s LIMIT 1", $user_id, $wpdb->prefix . 'capabilities');
     $roles = get_option($wpdb->prefix . 'user_roles');
@@ -215,10 +215,10 @@ function ifwp_user_can($user_id = 0, $capability = ''){
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 define('SHORTINIT', true);
-$abspath = ifwp_dirname(__FILE__, $_GET['levels']);
+$abspath = ldc_dirname(__FILE__, $_GET['levels']);
 $loader = $abspath . 'wp-load.php';
 if(!file_exists($loader)){
-    ifwp_404();
+    ldc_404();
 }
 require_once($loader);
 error_reporting(0);
@@ -227,27 +227,27 @@ $basedir = ABSPATH . 'wp-content/uploads';
 $subdir = isset($_GET['yyyy'], $_GET['mm']) ? '/' . $_GET['yyyy'] . '/' . $_GET['mm'] : '';
 $file = $basedir . $subdir . '/' . $_GET['file'];
 if(!is_file($file)){
-	ifwp_404();
+	ldc_404();
 }
-$post_id = ifwp_attachment_file_to_postid($file);
+$post_id = ldc_attachment_file_to_postid($file);
 if(!$post_id){
-    ifwp_serve_file($file);
+    ldc_serve_file($file);
 }
-$user_id = ifwp_get_current_user_id();
+$user_id = ldc_get_current_user_id();
 if(!$user_id){
-    ifwp_404();
+    ldc_404();
 }
-$post_status = ifwp_get_post_status($post_id);
+$post_status = ldc_get_post_status($post_id);
 if('publish' === $post_status){
-	ifwp_serve_file($file);
+	ldc_serve_file($file);
 }
-$post = ifwp_get_post($post_id);
+$post = ldc_get_post($post_id);
 if($user_id === $post->post_author){
-	ifwp_serve_file($file);
+	ldc_serve_file($file);
 }
-if(ifwp_current_user_can('read_private_posts')){
-	ifwp_serve_file($file);
+if(ldc_current_user_can('read_private_posts')){
+	ldc_serve_file($file);
 }
-ifwp_404();
+ldc_404();
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
